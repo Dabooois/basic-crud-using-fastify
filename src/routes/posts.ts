a150d6030571812d2d3e976d0c018698202bc016
Dabooois <cacayan.edcel26@gmail.com>
2023-04-25 12:16:58 +0800

    refactor(): move post database operation in service post

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,103 +1,94 @@
 import { IPost, IPostId } from '../types/posts';
-import { PrismaClient } from '@prisma/client';
 import { FastifyInstance } from 'fastify';
-const client = new PrismaClient();
+import postService from '../services/posts';
 
 const postRoute = async (fastify: FastifyInstance, _options: Object) => {
-  fastify.get('/', async function (request, reply) {
-    const posts = await client.post.findMany();
-    reply.status(200).send({
-      data: posts,
-    });
-  }),
-    fastify.post<{ Body: IPost }>('/post/new', async function (request, reply) {
-      const { title, body, isPublished } = request.body;
-      const convertToBoolean = isPublished === 'false' ? false : true;
-
-      await client.post.create({
-        data: {
-          title,
-          body,
-          isPublished: convertToBoolean,
-        },
-      });
-
-      reply.status(200).send({
-        message: 'Post successfully created',
-      });
+    fastify.get('/', async function (_request, reply) {
+        const { ok, results, message } = await postService.getPosts();
+        if (ok) {
+            reply.status(200).send({
+                results,
+            });
+        }
+        reply.status(400).send({
+            message,
+        });
     }),
-    fastify.get<{ Params: IPostId }>(
-      '/posts/:id/edit',
-      async (request, reply) => {
-        const { id } = request.params;
+        fastify.post<{ Body: IPost }>(
+            '/post/new',
+            async function (request, reply) {
+                const { ok, message } = await postService.createPost({
+                    ...request.body,
+                });
+                if (ok) {
+                    reply.status(200).send({
+                        message,
+                    });
+                }
+                reply.status(400).send({
+                    message,
+                });
+            }
+        ),
+        fastify.get<{ Params: IPostId }>(
+            '/posts/:id/edit',
+            async (request, reply) => {
+                const { id } = request.params;
 
-        try {
-          const result = await client.post.findUnique({
-            where: {
-              id: Number(id),
-            },
-          });
+                const { ok, result, message } = await postService.getPost(
+                    Number(id)
+                );
 
-          reply.status(200).send({
-            data: result,
-          });
-        } catch (error) {
-          reply.status(400).send({
-            message: 'Error in updating post',
-          });
-        }
-      }
-    ),
-    fastify.put<{ Params: IPostId; Body: IPost }>(
-      '/posts/:id',
-      async (request, reply) => {
-        const { id } = request.params;
-        const { title, body, isPublished } = request.body;
-        const convertToBoolean = isPublished === 'false' ? false : true;
+                if (ok) {
+                    reply.status(200).send({
+                        result,
+                    });
+                }
+                reply.status(400).send({
+                    message,
+                });
+            }
+        ),
+        fastify.put<{ Params: IPostId; Body: IPost }>(
+            '/posts/:id',
+            async (request, reply) => {
+                const { id } = request.params;
 
-        try {
-          await client.post.update({
-            where: {
-              id: Number(id),
-            },
-            data: {
-              title,
-              body,
-              isPublished: convertToBoolean,
-            },
-          });
+                const { ok, message } = await postService.updatePost(
+                    Number(id),
+                    {
+                        ...request.body,
+                    }
+                );
 
-          reply.status(200).send({
-            message: 'Post Successfully updated',
-          });
-        } catch (error) {
-          reply.status(400).send({
-            message: 'Error in updating post',
-          });
-        }
-      }
-    ),
-    fastify.delete<{ Params: IPostId }>(
-      '/posts/:id',
-      async (request, reply) => {
-        const { id } = request.params;
-        try {
-          await client.post.delete({
-            where: {
-              id: Number(id),
-            },
-          });
+                if (ok) {
+                    reply.status(200).send({
+                        message,
+                    });
+                }
+                reply.status(400).send({
+                    message,
+                });
+            }
+        ),
+        fastify.delete<{ Params: IPostId }>(
+            '/posts/:id',
+            async (request, reply) => {
+                const { id } = request.params;
+                const { ok, message } = await postService.deletePost(
+                    Number(id)
+                );
 
-          reply.status(200).send({
-            message: 'Post successfully deleted',
-          });
-        } catch (error) {
-          reply.status(400).send({
-            message: 'Error in deleting post',
-          });
-        }
-      }
-    );
+                if (ok) {
+                    reply.status(200).send({
+                        message,
+                    });
+                }
+                reply.status(400).send({
+                    message,
+                });
+            }
+        );
 };
 
 export default postRoute;
