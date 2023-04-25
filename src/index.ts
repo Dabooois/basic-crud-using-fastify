6b940b00c7fab1a828f2f54e7dd0964b50bf133f
Dabooois <cacayan.edcel26@gmail.com>
2023-04-25 13:05:50 +0800

    feat(): add graphql fastify-plugin and mecurius plugin

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,34 @@
 import fastify from 'fastify';
 import postRoute from './route/post';
+import mercurius from 'mercurius';
 
 const server = fastify().register(require('@fastify/formbody'));
 
 server.register(postRoute);
 
-server.listen({ port: 8080, host: '127.0.0.1' }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
+const schema = `
+  type Query {
+    hello: String
   }
-  console.log(`Server listening at ${address}`);
+`;
+
+// The root provides a resolver function for each API endpoint
+const resolvers = {
+    hello: () => {
+        return 'Hello world!';
+    },
+};
+
+server.register(mercurius, {
+    schema,
+    resolvers,
+    graphiql: true,
+});
+
+server.listen({ port: 8080, host: '127.0.0.1' }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
 });
